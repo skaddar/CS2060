9acccd49b85fc71d4f471d02a01ee79b14c7f7b9
skaddar <solimankaddar3@gmail.com>
2023-02-14 01:22:20 -0700

    first update of HW3... goodluck bro
    Just created the global constants as a headstart. Goodluck bro

---
+++ CS2060-Class-Code-main/Homework/KaddarSolimanHW3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+const double MIN_FLAT_FEE = 3.00;
+const unsigned int FLAT_RATE_MAX_HOURS = 3;
+const double HOURLY_CHARGE = 0.75;
+const double MAX_CHARGE = 12.00;
+
+int main(void)
+{
+	bool exitParking = false;
+	
+	do
+	{
+		puts("Welcome to the parking Garage!");
+		puts("How long will you park?");
+
+		double hours =
+
+	} while (exitParking == false);
+
+}
