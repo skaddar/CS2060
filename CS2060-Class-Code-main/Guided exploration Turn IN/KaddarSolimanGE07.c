98d706812f7d9082bdf9dfe84b350e72add0e555
skaddar <solimankaddar3@gmail.com>
2023-04-08 23:00:35 -0600

    Create KaddarSolimanGE07.c

---
+++ CS2060-Class-Code-main/Guided exploration Turn IN/KaddarSolimanGE07.c
@@ -0,0 +1,23 @@
+//
+// Name: Soliman Kaddar
+// Class: CS2060 (M/W)
+// Guided Exploration 07
+// Due:14 April 2023
+// 
+// Description: 
+// 
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct pet {
+
+    char name[80];
+    unsigned int age;
+
+}Pet;
